1060d394b2eabf96810e783e18ca5c51b4e7eaea
perbalu <dev.perbalu@gmail.com>
2022-04-04 16:33:22 +0200

    Product context

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,8 +2,7 @@ import { Routes, Route } from "react-router";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => <div>Hello</div>;
+import Shop from "./routes/shop/shop.component";
 
 function App() {
   return (
--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
